e3ec2f6ff2bfefc85279f905836630742e0dd4fa
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-12-06 08:01:00 +0900

    UIImagePickerの操作をハンドルするCoordinatorクラス

--- samples-UIViewRepresentable/Views/UIImagePickerControllerView.swift
+++ samples-UIViewRepresentable/Views/UIImagePickerControllerView.swift
@@ -9,6 +9,16 @@ import SwiftUI
 
 // UIImagePickerControllerをSwiftUIで使用するための実装
 struct UIImagePickerControllerView: UIViewRepresentable {
+
+    // UIImagePickerContorollerの操作をハンドルするためのコーディネータクラス
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: UIImagePickerControllerView // 親ビューへの参照
+
+        init(_ parent: UIImagePickerControllerView) {
+            self.parent = parent
+        }
+    }
+
     func makeUIView(context: Context) -> some UIView {
         <#code#>
     }
