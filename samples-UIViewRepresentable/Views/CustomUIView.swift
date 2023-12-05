3b50b2c37160858dae8f16d1852286a11d69afa1
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-12-06 07:43:44 +0900

    SwiftUIで使用するUIViewを定義

--- samples-UIViewRepresentable/Views/CustomUIView.swift
+++ samples-UIViewRepresentable/Views/CustomUIView.swift
@@ -5,7 +5,22 @@
 //  Created by Kensuke Nakagawa on 2023/12/06.
 //
 
+import SwiftUI
 import UIKit
 
+// SwiftUIで使用するUIViewを定義
+struct CustomUIView: UIViewRepresentable {
 
+    // ここでUIViewを生成
+    func makeUIView(context: Context) -> some UIView {
+        let view = UILabel()
+        view.text = "これはカスタムUILabelです"
+        return view
+    }
+
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        // UIViewの更新ロジック...
+    }
+}
 
