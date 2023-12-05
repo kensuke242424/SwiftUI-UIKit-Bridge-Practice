3955831c7f9cca824ea0a6eb29a40048b2e51dde
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-12-06 07:54:23 +0900

    UIImagePickerControllerView追加

---
+++ samples-UIViewRepresentable/Views/UIImagePickerControllerView.swift
@@ -0,0 +1,19 @@
+//
+//  UIImagePickerControllerView.swift
+//  samples-UIViewRepresentable
+//
+//  Created by Kensuke Nakagawa on 2023/12/06.
+//
+
+import SwiftUI
+
+// UIImagePickerControllerをSwiftUIで使用するための実装
+struct UIImagePickerControllerView: UIViewRepresentable {
+    func makeUIView(context: Context) -> some UIView {
+        <#code#>
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        <#code#>
+    }
+}
