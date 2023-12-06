11c2ff67a2978ca0581276ccf97b6f466454225e
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-12-06 09:39:00 +0900

    UIImagePickerControllerViewを使用するSwiftUIビュー追加

---
+++ samples-UIViewRepresentable/Views/UIImagePickerControllerViewRepresentable.swift
@@ -0,0 +1,19 @@
+//
+//  UIImagePickerControllerViewRepresentable.swift
+//  samples-UIViewRepresentable
+//
+//  Created by Kensuke Nakagawa on 2023/12/06.
+//
+
+import SwiftUI
+
+// UIImagePickerControllerViewを使用するSwiftUIビュー
+struct UIImagePickerControllerViewRepresentable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIImagePickerControllerViewRepresentable()
+}
